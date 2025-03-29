abe2af99ddb375e460b684e2d540320292a19ccc
Gennadiy <relect@bk.ru>
2025-03-29 13:05:46 +0300

    fix

--- client-management/src/main/java/com/edmebank/clientmanagement/controller/TestEmailController.java
+++ client-management/src/main/java/com/edmebank/clientmanagement/controller/TestEmailController.java
@@ -3,14 +3,13 @@ package com.edmebank.clientmanagement.controller;
 import com.edmebank.clientmanagement.model.Notification;
 import com.edmebank.clientmanagement.service.sending.MailSenderImpl;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.edmebank.clientmanagement.model.Notification.NotificationType.PASSPORT_EXPIRY;
 
-@Slf4j
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/send")
@@ -21,12 +20,11 @@ public class TestEmailController { // todo вынести в тесты
     @GetMapping
     public String sendEmail() {
         Notification notification = Notification.builder()
-                .email("murik311088@yandex.ru")
+                .email("relect@bk.ru")
                 .type(PASSPORT_EXPIRY)
                 .message("Simple message")
                 .build();
         mailSender.send(notification);
-        log.info("Email sent to {}", "murik311088@yandex.ru");
         return "Email sent successfully";
     }
 }
