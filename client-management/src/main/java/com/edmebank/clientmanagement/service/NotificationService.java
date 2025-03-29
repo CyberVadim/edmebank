2e814895078579dcb0ff0912ce993938176af059
Gennadiy <relect@bk.ru>
2025-03-29 11:52:36 +0300

    refactor

--- client-management/src/main/java/com/edmebank/clientmanagement/service/NotificationService.java
+++ client-management/src/main/java/com/edmebank/clientmanagement/service/NotificationService.java
@@ -1,102 +1,80 @@
 package com.edmebank.clientmanagement.service;
 
-import com.edmebank.clientmanagement.exception.VpnActiveException;
 import com.edmebank.clientmanagement.model.Client;
-import com.edmebank.clientmanagement.model.notification.Notification;
-import com.edmebank.clientmanagement.model.notification.Notification.NotificationStatus;
-import com.edmebank.clientmanagement.model.notification.Notification.NotificationType;
+import com.edmebank.clientmanagement.model.Notification;
+import com.edmebank.clientmanagement.model.Notification.NotificationStatus;
+import com.edmebank.clientmanagement.model.Notification.NotificationType;
 import com.edmebank.clientmanagement.repository.ClientRepository;
 import com.edmebank.clientmanagement.repository.NotificationRepository;
-import jakarta.mail.internet.MimeMessage;
+import com.edmebank.clientmanagement.service.sending.Sender;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class NotificationService {
 
-    private final JavaMailSender mailSender;
+    private final List<Sender> senders;
     private final NotificationRepository notificationRepository;
     private final ClientRepository clientRepository;
+    private final ExecutorService executorService;
 
-    @Value("${spring.mail.username}")
-    private String senderEmail;
-
-    @Autowired
-    public NotificationService(NotificationRepository notificationRepository, ClientRepository clientRepository, JavaMailSender mailSender) {
-        this.notificationRepository = notificationRepository;
-        this.clientRepository = clientRepository;
-        this.mailSender = mailSender;
-    }
-
-    @Scheduled(cron = "0 0 8 * * ?")
-    public void processNotifications() {
-        log.info("Processing notifications");
-        createPassportExpiryNotifications();
-    }
-
-    @Scheduled(cron = "0 0 9 * * ?")
-    public void sendNotifications() {
-        log.info("Sending notifications");
-        sendNotification();
-        resendUnconfirmedNotifications();
-    }
+    @Value("${notification.passport_expire.before_months}")
+    private int notificationBeforeMonths;
+    @Value("${notification.max_attempt}")
+    private int maxAttempt;
+    @Value("${server.port}")
+    private int serverPort;
 
     public void createPassportExpiryNotifications() {
-        LocalDate expiryThreshold = LocalDate.now().plusMonths(3).plusDays(1);
+        LocalDate expiryThreshold = LocalDate.now().plusMonths(notificationBeforeMonths);
         List<Client> clients = clientRepository.findByPassportExpiryDateBefore(expiryThreshold);
 
         for (Client client : clients) {
-            boolean alreadyNotified = notificationRepository.findByClientId(client.getId()).stream()
-                    .anyMatch(n -> n.getType() == NotificationType.PASSPORT_EXPIRY);
-            if (!alreadyNotified) {
+            Optional<Notification> optionalNotification = notificationRepository.findFirstByClientIdAndType(client.getId(), NotificationType.PASSPORT_EXPIRY);
+            if (optionalNotification.isEmpty()) {
+
                 Notification notification = Notification.builder()
                         .clientId(client.getId())
                         .email(client.getEmail())
-                        .message("Ваш паспорт скоро истекает. Пожалуйста, обновите его.")
+                        .message("NotNull")  // todo Marchenko есть другие идеи?
                         .type(NotificationType.PASSPORT_EXPIRY)
                         .status(NotificationStatus.PENDING)
                         .createdAt(Instant.now())
                         .attemptCount(0)
                         .build();
+                notification = notificationRepository.save(notification);
+
+                String message = String.format("""
+                Ваш паспорт истекает %s. Пожалуйста, обновите его.
+                Для подтверждения уведомления перейдите по ссылке:
+                http://localhost:%d/api/v1/notifications/confirm?notificationId=%d.
+                Чтобы отписаться от уведомлений пройдите по ссылке:
+                http://localhost:%d
+                """, client.getPassportExpiryDate(), serverPort, notification.getId(), serverPort);
+                //todo Marchenko есть идеи как применить один параметр дважды? и что думаете насчёт localhost - домена же у нас нет?
+
+                notification.setMessage(message);
                 notificationRepository.save(notification);
             }
         }
     }
 
-    void resendUnconfirmedNotifications() {
-        List<Notification> notifications = notificationRepository.findByStatusAndCreatedAtBefore(
-                NotificationStatus.SENT, Instant.now().minus(3, ChronoUnit.DAYS));
-
-        for (Notification notification : notifications) {
-            if (!notification.isClientConfirmed() && notification.getAttemptCount() < 3) {
-                sendEmail(notification.getEmail(), generateSubject(notification.getType()), notification.getMessage(), true);
-                notification.setLastAttemptAt(Instant.now());
-                notification.setAttemptCount(notification.getAttemptCount() + 1);
-                notificationRepository.save(notification);
-            }
-        }
-    }
-
-    void sendNotification() {
+    public void sendNotification() {
         List<Notification> notifications = notificationRepository.findByStatus(NotificationStatus.PENDING);
         for (Notification notification : notifications) {
-            sendEmail(notification.getEmail(), generateSubject(notification.getType()), notification.getMessage(), true);
+            senders.forEach(sender -> executorService.submit(() -> sender.send(notification)));
             notification.setStatus(NotificationStatus.SENT);
             notification.setLastAttemptAt(Instant.now());
             notification.setAttemptCount(notification.getAttemptCount() + 1);
@@ -104,72 +82,29 @@ public class NotificationService {
         }
     }
 
+    public void resendUnconfirmedNotifications() {
+        Instant lastAttemptBefore = Instant.now().minus(3, ChronoUnit.DAYS);
+        List<Notification> notifications =
+                notificationRepository.findByStatusAndLastAttemptAtBeforeAndAttemptCountLessThan(
+                NotificationStatus.SENT, lastAttemptBefore, maxAttempt);
 
-    private String generateSubject(NotificationType type) {
-        return switch (type) {
-            case PASSPORT_EXPIRY -> "Требуется замена паспорта";
-            case PRODUCT_UPDATE -> "Изменение условий обслуживания";
-            case DEPOSIT_INTEREST -> "Начисление процентов по депозиту";
-        };
-    }
-
-    @Async
-    public void sendEmail(String to, String subject, String text, boolean isHtml) {
-        if (isVpnActive()) {
-            log.warn("Ошибка: Отключите VPN и попробуйте снова.");
-            throw new VpnActiveException();
+        for (Notification notification : notifications) {
+            senders.forEach(sender -> sender.send(notification));
+            notification.setLastAttemptAt(Instant.now());
+            notification.setAttemptCount(notification.getAttemptCount() + 1);
+            notificationRepository.save(notification);
         }
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-            helper.setFrom(senderEmail);
-            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(text, isHtml);
-            mailSender.send(message);
-            log.info("Email sent to {}", to);
-        } catch (Exception e) {
-            log.error("Failed to send email to {}", to, e);
-        }
-    }
-
-    public List<Notification> getNotificationHistory(UUID clientId) {
-        return notificationRepository.findByClientId(clientId);
     }
 
     public void confirmNotification(Long notificationId) {
         notificationRepository.findById(notificationId).ifPresent(notification -> {
-            notification.setClientConfirmed(true);
+            notification.setStatus(NotificationStatus.DELIVERED);
             notification.setClientResponseDate(Instant.now());
             notificationRepository.save(notification);
         });
     }
 
-    private boolean isVpnActive() {
-        return isSiteAccessible("https://www.rutracker.org");
-    }
-
-    private boolean isSiteAccessible(String siteUrl) {
-        try {
-            URL url = new URL(siteUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(3000); // 3 секунды
-            connection.setReadTimeout(3000);
-            int responseCode = connection.getResponseCode();
-
-            if (responseCode == 200) {
-                log.warn("VPN detected! {} is accessible.", siteUrl);
-                return true; // VPN включен, так как сайт открылся
-            }
-        } catch (IOException e) {
-            log.info("VPN is likely off. {} is not accessible.", siteUrl);
-        }
-        return false;
+    public List<Notification> getNotificationHistory(UUID clientId) {
+        return notificationRepository.findByClientId(clientId);
     }
 }
-
-
-
-
-
