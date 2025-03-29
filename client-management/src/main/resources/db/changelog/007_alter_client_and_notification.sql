310699ec1f401be4efdd138654d22cc14aeda37a
Gennadiy <relect@bk.ru>
2025-03-29 12:58:15 +0300

    fix

--- client-management/src/main/resources/db/changelog/007_alter_client_and_notification.sql
+++ client-management/src/main/resources/db/changelog/007_alter_client_and_notification.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 --changeset Marchenko:007
---preconditions onFail:CONTINUE onError:CONTINUE
+--preconditions onFail:HALT onError:HALT
 
 ALTER TABLE cm_schema.clients ADD COLUMN enable_notifications BOOLEAN NOT NULL DEFAULT true;
 ALTER TABLE cm_schema.notifications DROP COLUMN client_confirmed;
