58e38e7aab4ab12ba051eb0f5d4c04226d199f33
Gennadiy <relect@bk.ru>
2025-04-14 12:47:02 +0300

    [EDMEBANK-23-2.1] test(ClientController): тестирование контроллера клиента

---
+++ client-management/src/test/java/com/edmebank/clientmanagement/controller/ClientControllerTest.java
@@ -0,0 +1,55 @@
+package com.edmebank.clientmanagement.controller;
+
+import com.edmebank.clientmanagement.service.ClientService;
+import com.edmebank.clientmanagement.service.NotificationService;
+import com.edmebank.clientmanagement.service.SpectrumService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ClientController.class)
+public class ClientControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private ClientService clientService;
+    @MockitoBean
+    private SpectrumService spectrumService;
+    @MockitoBean
+    private NotificationService notificationService;
+
+    @Test
+    public void testPatchClient() throws Exception {
+        String systemId = "RequestService";
+        String requestBody = "{\"firstName\":\"Иса\", \"lastName\":\"Исаев\", \"email\":\"example@mail.ru\"}";
+
+        mockMvc.perform(patch("/api/v1/clients/10000000-0000-0000-0000-000000000201/patch")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("SystemId", systemId)
+                .header("Content-Length", 10)
+                .content(requestBody))
+
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+    @Test
+    public void testPatchClientWithNotHeader() throws Exception {
+        String systemId = "RequestService";
+        String requestBody = "{\"firstName\":\"Иса\", \"lastName\":\"Исаев\", \"email\":\"example@mail.ru\"}";
+
+        mockMvc.perform(patch("/api/v1/clients/10000000-0000-0000-0000-000000000201/patch")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Content-Length", 10)
+                        .content(requestBody))
+
+                .andExpect(status().isNotFound());
+    }
+}
