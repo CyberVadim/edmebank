6f16f593e432a3c3280591c0c098f94fd07d5b55
Anton Timoshenko <anton.timoshenko@quadcode.com>
2025-05-07 13:15:54 +0300

    # EDMEBANK-51: create springboot app

---
+++ src/main/java/ru/edmebank/EdmebankApplication.java
@@ -0,0 +1,11 @@
+package ru.edmebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdmebankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EdmebankApplication.class, args);
+    }
+}
