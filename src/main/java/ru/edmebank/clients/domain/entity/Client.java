09602a2dab0ad25582ddba78353d109e2f74975a
Anton Timoshenko <anton.timoshenko@quadcode.com>
2025-05-09 23:28:37 +0300

    # EDMEBANK-51:

--- src/main/java/ru/edmebank/clients/domain/entity/Client.java
+++ src/main/java/ru/edmebank/clients/domain/entity/Client.java
@@ -1,6 +1,5 @@
 package ru.edmebank.clients.domain.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +7,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -62,7 +62,8 @@ public class Client {
     @Column(nullable = false, length = 14, unique = true)
     private String snils;
 
-    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    @OneToOne
+    @JoinColumn(name = "client_security_info_id", referencedColumnName = "id")
     private ClientSecurityInfo securityInfo;
 
     @Column(precision = 20, scale = 2)
--- src/main/java/ru/edmebank/clients/domain/entity/ClientSecurityInfo.java
+++ src/main/java/ru/edmebank/clients/domain/entity/ClientSecurityInfo.java
@@ -2,9 +2,9 @@ package ru.edmebank.clients.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -18,11 +18,10 @@ import java.util.UUID;
 public class ClientSecurityInfo {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @MapsId
-    @OneToOne
-    @JoinColumn(name = "id")
+    @OneToOne(mappedBy = "securityInfo")
     private Client client;
 
     @Column(nullable = false)
