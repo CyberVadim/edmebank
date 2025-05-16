05118fb23ba3885bae6b6bdf6ee4e7815c789781
Marchenko <relect@bk.ru>
2025-05-16 13:31:21 +0300

    #EDMEBANK-55-be-documents : add strategy id

--- src/main/java/ru/edmebank/clients/domain/entity/Documents.java
+++ src/main/java/ru/edmebank/clients/domain/entity/Documents.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -25,6 +27,7 @@ import java.util.UUID;
 public class Documents {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     public UUID id;
 
     @ManyToOne
