c0ce4916f5e8bc792e7439949263c182148771b0
Marchenko <relect@bk.ru>
2025-05-16 12:46:16 +0300

    #EDMEBANK-55-be-documents : документы клиента

---
+++ src/main/java/ru/edmebank/clients/domain/entity/Documents.java
@@ -0,0 +1,41 @@
+package ru.edmebank.clients.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import ru.edmebank.contracts.enums.DocumentType;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Documents {
+
+    @Id
+    public UUID id;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    public Client client;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 30)
+    public DocumentType type;
+
+    @Column(columnDefinition = "TEXT")
+    public String storageUrl;
+    public LocalDateTime uploadedAt;
+}
