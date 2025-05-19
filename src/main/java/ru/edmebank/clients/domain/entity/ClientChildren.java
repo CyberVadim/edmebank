369e82e02241e2b816d44e9d24b7ac14a4875915
Липская Александра Александровна <lipskaya2001@bk.ru>
2025-05-19 10:50:21 +0300

    EDMEBANK-56-be-client_children: solved conflict
    EDMEBANK-56-be-client_children: add updateAt
    
    EDMEBANK-56-be-client_children: delete shared import

--- src/main/java/ru/edmebank/clients/domain/entity/ClientChildren.java
+++ src/main/java/ru/edmebank/clients/domain/entity/ClientChildren.java
@@ -1,14 +1,23 @@
 package ru.edmebank.clients.domain.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import ru.edmebank.contracts.enums.RelationTypeEnum;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
-
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -37,5 +46,9 @@ public class ClientChildren {
             columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     @Setter(AccessLevel.NONE)
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime updatedAt;
 
 }
