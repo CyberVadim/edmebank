1a64334b5fd2a63c5469cbff75c3f8a6ce1df257
Липская Александра Александровна <lipskaya2001@bk.ru>
2025-05-19 13:15:24 +0300

    EDMEBANK-56-be-client_children: solved conflict
    EDMEBANK-56: resolved conflict in master-changelog.xml

--- src/main/resources/db/migration/013-insert-client-categories-table.sql
+++ src/main/resources/db/migration/013-insert-client-categories-table.sql
@@ -2,6 +2,6 @@
 --preconditions onFail:HALT onError:HALT
 --changeset Elena_Yankovskaya:005
 
-INSERT INTO client_categories (id, category, loyalty_points, last_category_update)
-    VALUES ('3f5c9f78-0c9a-4c1e-940d-17a2b7e232b1', 'VIP', 1500, '2023-11-01 00:00:00')
+INSERT INTO client_categories (id, category, loyalty_points, create_at, update_at)
+    VALUES ('3f5c9f78-0c9a-4c1e-940d-17a2b7e232b1', 'VIP', 1500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
     ON CONFLICT (id) DO NOTHING;
