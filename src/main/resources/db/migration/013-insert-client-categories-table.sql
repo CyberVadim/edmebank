10eb79f0331c7e08f717f3f38348afdddeae4aab
e.yankovskaya <elena.parokhina@gmail.com>
2025-05-19 10:53:45 +0200

    EDMEBANK-82-bug-013-insert-client-categories-table.sql: устранение Bug миграция 013-insert-client-categories-table.sql

--- src/main/resources/db/migration/013-insert-client-categories-table.sql
+++ src/main/resources/db/migration/013-insert-client-categories-table.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --preconditions onFail:HALT onError:HALT
---changeset Elena_Yankovskaya:005
+--changeset Elena_Yankovskaya:013
 
-INSERT INTO client_categories (id, category, loyalty_points, create_at, update_at)
-    VALUES ('3f5c9f78-0c9a-4c1e-940d-17a2b7e232b1', 'VIP', 1500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+INSERT INTO client_categories (id, category, loyalty_points, created_at, updated_at)
+    VALUES ('3f5c9f78-0c9a-4c1e-940d-17a2b7e232b1', 'VIP', 1500, '2023-11-01 00:00:00', CURRENT_TIMESTAMP)
     ON CONFLICT (id) DO NOTHING;
