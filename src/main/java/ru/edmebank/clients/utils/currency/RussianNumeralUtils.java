07faaed7ab21959e7f2f4721cafa0ab788d98de8
e.yankovskaya <elena.parokhina@gmail.com>
2025-05-26 10:57:25 +0200

    EDMEBANK-89: fix import static, add @UtilityClass

--- src/main/java/ru/edmebank/clients/utils/currency/RussianNumeralUtils.java
+++ src/main/java/ru/edmebank/clients/utils/currency/RussianNumeralUtils.java
@@ -1,14 +1,22 @@
 package ru.edmebank.clients.utils.currency;
 
 import com.github.petrovich4j.Gender;
+import lombok.experimental.UtilityClass;
 
+import static com.github.petrovich4j.Gender.Female;
+import static java.lang.Math.abs;
+
+@UtilityClass
 public class RussianNumeralUtils {
     public static String declension(int number, String formZero, String[] forms) {
-        int n = Math.abs(number) % 100;
+        int n = abs(number) % 100;
+
         if (n >= 11 && n <= 19) {
             return forms[1];
         }
+
         int i = n % 10;
+
         return switch (i) {
             case 1 -> formZero;
             case 2, 3, 4 -> forms[0];
@@ -17,7 +25,7 @@ public class RussianNumeralUtils {
     }
 
     public static String correctGender(String input, Gender gender) {
-        return (gender == Gender.Female) ?
+        return (gender == Female) ?
                 input.replaceAll("\\bодин\\b$", "одна").replaceAll("\\bдва\\b$", "две") :
                 input;
     }
