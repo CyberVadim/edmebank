87d718399b8c81e9bea39bb3b14e34e8c3f92db4
Marchenko <relect@bk.ru>
2025-05-29 12:07:02 +0300

    #EDMEBANK-81-be : add create client

---
+++ src/test/java/ru/edmebank/clients/adapter/input/ui/ClientAdapterTest.java
@@ -0,0 +1,39 @@
+package ru.edmebank.clients.adapter.input.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ClientAdapterTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    ClassLoader loader = getClass().getClassLoader();
+
+    @Test
+    void completeCreateClientTest() throws Exception {
+        String jsonContent = Files.readString(Path.of(loader.getResource("test-data/clients/create_client.json").toURI()),
+                StandardCharsets.UTF_8);
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/ui/client")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonContent)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+}
