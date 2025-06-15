1f7a519c37d596928c76828cf5b261de300ad88e
Vladimir Kapyrin <garage@yanex.ru>
2025-06-15 14:32:31 +0100

    EDMEBANK-64: Complete migration and refactoring

---
+++ src/main/resources/db/migration/041-insert-account-product-links-table.sql
@@ -0,0 +1,9 @@
+INSERT INTO account_product_links (
+    account_id,
+    client_product_id
+) VALUES
+    -- Связь: Текущий счёт с продуктом клиента
+    (
+        'd3ffbe44-5f3c-4e4a-b1a0-9ee9ef390d44',
+        '3f5c9f78-0c9a-4c1e-940d-17a2b7e232bb'
+    );
