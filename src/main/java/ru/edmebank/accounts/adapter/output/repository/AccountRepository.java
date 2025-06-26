9c556528d06d8f05330dcc61e892c6c907893a96
”Vadim <protasoff61@gmail.com>
2025-06-26 15:10:11 +0400

    EDMEBANK-139

--- src/main/java/ru/edmebank/accounts/adapter/output/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.edmebank.accounts.adapter.output.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.edmebank.accounts.domain.entity.Account;
-import java.util.UUID;
-
-public interface AccountRepository extends JpaRepository<Account, UUID> {
-}
