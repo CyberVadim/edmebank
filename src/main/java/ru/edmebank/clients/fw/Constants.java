fee1e80cfb779597a2f9c70bbe91e8c9d735cfc8
Koltsov Artur <artur-koltsov@list.ru>
2025-06-27 08:44:41 +0300

    #EDMEBANK-121 delete no use

--- src/main/java/ru/edmebank/clients/fw/Constants.java
+++ src/main/java/ru/edmebank/clients/fw/Constants.java
@@ -6,14 +6,10 @@ public class Constants {
     public static final String REGEX_PASSWORD_HAS_SPECIAL_CHARACTERS = "^(?=.*[!#$%&'*+-/=?^_`{|}~.]).*$";
     public static final String REGEX_PASSWORD_ALLOW = "^[0-9a-zA-Z!#$%&'*+-/=?^_`{|}~.]{8,}$";
     public static final String REGEX_CARD_NUMBER = "^\\d{16,19}$";
-    public static final String REGEX_CVV = "^[0-9]{3,4}$";
     public static final String REGEX_EXPIRY_DATE = "^(0[1-9]|1[0-2])/([0-9]{2})$";
     public static final String REGEX_IBAN = "^[A-Z]{2}[0-9]{2}[A-Z0-9]{4,30}$";
-    public static final String REGEX_RUS_ACCOUNT = "^[0-9]{20}$";
     public static final String REGEX_CORRESPONDENT_ACCOUNT = "^301[0-9]{20}$";
-    public static final String REGEX_BIC = "^[0-9]{9}$";
     public static final String REGEX_FORBIDDEN_CHARS = "[<>\\|]";
-    public static final String REGEX_CONFIRMATION_CODE = "^[0-9]{6}$";
 
     public static final String[] FORBIDDEN_PHRASES = {"шутка", "ха-ха", "блокируй", "тест", "прикол"};
 
